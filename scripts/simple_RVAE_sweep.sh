2efc0a9e50606bdf9b41364dfb08e0c495a3f3f7
Sam Coope <sam.j.coope@gmail.com>
2017-04-22 22:29:19 +0200

    Fix bug in scripts

--- scripts/simple_RVAE_sweep.sh
+++ scripts/simple_RVAE_sweep.sh
@@ -2,19 +2,19 @@
 
 COMMAND='python experiments/Recurrent_VAE_baseline/experiment_128k.py'
 
-$COMMAND simple_layer_gru_8
-./scripts/gdrive upload experiments/Recurrent_VAE_baseline/simple_layer_gru_8
+$COMMAND single_layer_gru_8
+./scripts/gdrive upload experiments/Recurrent_VAE_baseline/single_layer_gru_8
 
 echo "First experiment finished" | mail -s 'Masters Project Bot' sam.j.coope@gmail.com
-$COMMAND simple_layer_gru_16
-./scripts/gdrive upload experiments/Recurrent_VAE_baseline/simple_layer_gru_16
-$COMMAND simple_layer_gru_32
-./scripts/gdrive upload experiments/Recurrent_VAE_baseline/simple_layer_gru_32
-$COMMAND simple_layer_gru_64
-./scripts/gdrive upload experiments/Recurrent_VAE_baseline/simple_layer_gru_64
-$COMMAND simple_layer_gru_128
-./scripts/gdrive upload experiments/Recurrent_VAE_baseline/simple_layer_gru_128
-$COMMAND simple_layer_gru_256
-./scripts/gdrive upload experiments/Recurrent_VAE_baseline/simple_layer_gru_256
+$COMMAND single_layer_gru_16
+./scripts/gdrive upload experiments/Recurrent_VAE_baseline/single_layer_gru_16
+$COMMAND single_layer_gru_32
+./scripts/gdrive upload experiments/Recurrent_VAE_baseline/single_layer_gru_32
+$COMMAND single_layer_gru_64
+./scripts/gdrive upload experiments/Recurrent_VAE_baseline/single_layer_gru_64
+$COMMAND single_layer_gru_128
+./scripts/gdrive upload experiments/Recurrent_VAE_baseline/single_layer_gru_128
+$COMMAND single_layer_gru_256
+./scripts/gdrive upload experiments/Recurrent_VAE_baseline/single_layer_gru_256
 
 echo "Experiments finished." | mail -s 'Masters Project Bot' sam.j.coope@gmail.com
--- scripts/test_sweep.sh
+++ scripts/test_sweep.sh
@@ -2,7 +2,7 @@
 
 COMMAND='python experiments/Recurrent_VAE_baseline/experiment_128k.py'
 
-$COMMAND simple_layer_gru_8
-./scripts/gdrive upload experiments/Recurrent_VAE_baseline/simple_layer_gru_8
+$COMMAND single_layer_gru_8
+./scripts/gdrive upload experiments/Recurrent_VAE_baseline/single_layer_gru_8
 
 echo "First experiment finished" | mail -s 'Masters Project Bot' sam.j.coope@gmail.com
